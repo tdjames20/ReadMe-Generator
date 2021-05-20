0ee0c41888134cae762c5e5c2f6e357e2f1a862f
Trey James <treyjames@Treys-MacBook-Air.local>
2021-05-20 18:15:07 -0400

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,32 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+// TODO: Create an array of questions for user input
+const questions = ([
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Describe your project",
+        name: "description"
+    }
+    {
+        type: 
+    }
+
+]).then(answers => {
+
+})
+;
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
